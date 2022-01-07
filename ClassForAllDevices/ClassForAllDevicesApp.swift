98dafeaa44f06c587754a8f194cfbcea47dd23ed
Valery <ihValery@icloud.com>
2022-01-08 00:23:48 +0300

    Initial Commit

---
+++ ClassForAllDevices/ClassForAllDevicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassForAllDevicesApp.swift
+//  ClassForAllDevices
+//
+//  Created by Валерий Игнатьев on 08.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct ClassForAllDevicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
